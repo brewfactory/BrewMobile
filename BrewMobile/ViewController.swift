e1c6f15276a26602488f198bafa6e8db40dfd53c
Vásárhelyi Ágnes <agnes.vasarhelyi@sanomamedia.hu>
2014-08-19 11:12:45 +0200

    Initial Commit

---
+++ BrewMobile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BrewMobile
+//
+//  Created by Ágnes Vásárhelyi on 19/08/14.
+//  Copyright (c) 2014 Ágnes Vásárhelyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
