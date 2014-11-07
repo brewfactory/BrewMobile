5e1784b356665e9bb339a395db50328677948441
Agnes Vasarhelyi <agnes.vasarhelyi@sanomamedia.hu>
2014-11-07 20:30:35 +0100

    Created designer view controller class along with its storyboard representation.

---
+++ BrewMobile/BrewDesignerViewController.swift
@@ -0,0 +1,26 @@
+//
+//  BrewDesignerViewController.swift
+//  BrewMobile
+//
+//  Created by Ágnes Vásárhelyi on 07/11/14.
+//  Copyright (c) 2014 Ágnes Vásárhelyi. All rights reserved.
+//
+
+import UIKit
+
+class BrewDesignerViewController : UIViewController {
+
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var startTimeTextField: UITextField!
+    @IBOutlet weak var phasesTableView: UITableView!
+    var datePicker: UIDatePicker!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
