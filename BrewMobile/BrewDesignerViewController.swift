a4387ae2cff353db11bcb6cf516382eee2fbc5e7
Agnes Vasarhelyi <agnes.vasarhelyi@sanomamedia.hu>
2014-11-07 21:53:13 +0100

    Created action for date picker and text fields

--- BrewMobile/BrewDesignerViewController.swift
+++ BrewMobile/BrewDesignerViewController.swift
@@ -8,19 +8,51 @@
 
 import UIKit
 
-class BrewDesignerViewController : UIViewController {
+class BrewDesignerViewController : UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var startTimeTextField: UITextField!
     @IBOutlet weak var phasesTableView: UITableView!
-    var datePicker: UIDatePicker!
+    @IBOutlet weak var startTimePicker: UIDatePicker!
 
+    var name: String
+    var startTime: String
+    var phases: PhaseArray
+
+    required init(coder aDecoder: NSCoder) {
+        name = ""
+        startTime = ""
+        phases = []
+        super.init(coder: aDecoder)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    //MARK: UITextFieldDelegate
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if (textField == startTimeTextField) {
+            let nowDate = NSDate()
+            startTimePicker.minimumDate = nowDate
+            startTimePicker.date = nowDate
+            self.view.addSubview(startTimePicker)
+        }
+    }
+    
+    //MARK: IBAction methods
+
+    @IBAction func datePickerDateDidChange(datePicker: UIDatePicker) {
+        let dateFormatter = NSDateFormatter()
+        dateFormatter.dateFormat = "YYYY.MM.dd. HH:mm"
+        startTimeTextField.text = dateFormatter.stringFromDate(datePicker.date)
+        let isoDateFormatter = ISO8601DateFormatter()
+        startTime = isoDateFormatter.stringFromDate(datePicker.date)
     }
     
 }
