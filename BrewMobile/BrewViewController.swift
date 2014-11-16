7cef9d9b6ad448c025dd1033fe4f196fbbcd536c
Vasarhelyi Agi <vasarhelyi.agi@gmail.com>
2014-11-16 18:20:26 +0100

    Fixed showing name and start time on future brews as well.

--- BrewMobile/BrewViewController.swift
+++ BrewMobile/BrewViewController.swift
@@ -87,7 +87,7 @@ class BrewViewController: UIViewController, UITableViewDelegate, UITableViewData
     
     func updateNameLabel() {
         if self.actState.phases.count > 0 {
-            self.nameLabel.text = self.actState.inProgress ? "Brewing \(self.actState.name) at" : ""
+            self.nameLabel.text = "Brewing \(self.actState.name) at"
         } else {
             self.nameLabel.text = "We are not brewing :(\nHow is it possible?"
         }
@@ -98,7 +98,7 @@ class BrewViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
     func updateStartTimeLabel() {
-        self.startTimeLabel.text = self.actState.inProgress ? "started at \(self.actState.startTime)" : ""
+        self.startTimeLabel.text = "starting \(self.actState.startTime)"
     }
     
     func stateText(brewPhase: BrewPhase) -> String {
@@ -126,10 +126,10 @@ class BrewViewController: UIViewController, UITableViewDelegate, UITableViewData
         let cell = tableView.dequeueReusableCellWithIdentifier("BrewCell", forIndexPath: indexPath) as BrewCell
         if self.actState.phases.count > indexPath.row  {
             let brewPhase = self.actState.phases[indexPath.row]
-        
+            
             cell.minLabel.text = "\(brewPhase.min) minutes at \(Int(brewPhase.temp)) ˚C"
             cell.statusLabel.text = "\(self.stateText(brewPhase))"
-           
+            
             UIView.animateWithDuration(0.3, animations: { () -> Void in
                 cell.backgroundColor = brewPhase.state.bgColor()
                 cell.setTextColorForAllLabels(brewPhase.state == State.INACTIVE ? UIColor.blackColor() : UIColor.whiteColor())
