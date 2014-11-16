229bc0d7f8724e653c61319dbe95a3ca44d11d67
Vasarhelyi Agi <vasarhelyi.agi@gmail.com>
2014-11-16 17:35:10 +0100

    Refactored APIManager a bit.

--- BrewMobile/Helpers/APIManager.swift
+++ BrewMobile/Helpers/APIManager.swift
@@ -10,25 +10,46 @@ import Foundation
 
 class APIManager {
     class func createBrew(brew: BrewState) {
-        var request : NSMutableURLRequest = NSMutableURLRequest()
-        request.URL = NSURL(string: host + "api/brew")
-        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-        request.HTTPMethod = "POST"
-        
-        var error: AutoreleasingUnsafeMutablePointer<NSError?> = nil
-        
         if let brewJSON: JSON? = BrewState.encode(brew) {
-            request.HTTPBody = NSJSONSerialization.dataWithJSONObject(brewJSON!, options: NSJSONWritingOptions.PrettyPrinted, error: error)
-            
-            NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler:{ (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
-                
-                if(data != nil) {
-                    let jsonResult: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: nil)!
-                
-                    println(jsonResult)
-                }
-            })
+            postRequestWithBody(brewJSON!)
         }
     }
     
+    class func postRequestWithBody(body: JSON) {
+        var request : NSMutableURLRequest = NSMutableURLRequest()
+        var serializationError: NSError?
+
+        request.URL = NSURL(string: host + "api/brew")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.HTTPMethod = "POST"
+        request.HTTPBody = NSJSONSerialization.dataWithJSONObject(body, options: NSJSONWritingOptions.PrettyPrinted, error: &serializationError)
+
+        if serializationError == nil {
+           sendRequest(request)
+        } else {
+            println("Error serializing JSON from brew")
+        }
+    }
+    
+    class func sendRequest(request: NSMutableURLRequest) {
+        NSURLConnection.sendAsynchronousRequest(request, queue: NSOperationQueue(), completionHandler: {
+            (response:NSURLResponse!, data: NSData!, error: NSError!) -> Void in
+            
+            var serializationError: NSError?
+            if error == nil {
+                if data != nil {
+                    if let jsonResult: AnyObject = NSJSONSerialization.JSONObjectWithData(data, options:NSJSONReadingOptions.MutableContainers, error: &serializationError) {
+                        if serializationError == nil {
+                            println(jsonResult)
+                        } else {
+                            println("Error serializing JSON from brew")
+                        }
+                    }
+                }
+            } else {
+                println("Error during request \(error.localizedDescription)")
+            }
+        })
+    }
+
 }
