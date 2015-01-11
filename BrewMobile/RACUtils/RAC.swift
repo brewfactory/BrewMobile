5d5f435db0dcd22f2ad99c5fe3906ac424bd6982
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-01-11 14:29:40 +0100

    Added RAC utils.

---
+++ BrewMobile/RACUtils/RAC.swift
@@ -0,0 +1,33 @@
+//
+//  RAC.swift
+//  ReactiveSwiftFlickrSearch
+//
+//  Created by Colin Eberhardt on 15/07/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+
+// a struct that replaces the RAC macro
+struct RAC  {
+  var target : NSObject!
+  var keyPath : String!
+  var nilValue : AnyObject!
+  
+  init(_ target: NSObject!, _ keyPath: String, nilValue: AnyObject? = nil) {
+    self.target = target
+    self.keyPath = keyPath
+    self.nilValue = nilValue
+  }
+  
+  
+  func assignSignal(signal : RACSignal) {
+    signal.setKeyPath(self.keyPath, onObject: self.target, nilValue: self.nilValue)
+  }
+}
+
+infix operator ~> {}
+func ~> (signal: RACSignal, rac: RAC) {
+  rac.assignSignal(signal)
+}
---
+++ BrewMobile/RACUtils/RACObserve.swift
@@ -0,0 +1,15 @@
+//
+//  RACObserve.swift
+//  ReactiveSwiftFlickrSearch
+//
+//  Created by Colin Eberhardt on 15/07/2014.
+//  Copyright (c) 2014 Colin Eberhardt. All rights reserved.
+//
+
+import Foundation
+import ReactiveCocoa
+
+// replaces the RACObserve macro
+func RACObserve(target: NSObject!, keyPath: String) -> RACSignal  {
+  return target.rac_valuesForKeyPath(keyPath, observer: target)
+}
