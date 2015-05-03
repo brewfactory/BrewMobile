ab3e4e46f74e466dd7a7c3c59aae6b660b02ae8b
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-05-03 11:38:58 +0200

    Removed travis config, still not working

--- sign_fw.sh
+++
@@ -1,9 +0,0 @@
-FRAMEWORK_DIR="./Carthage/Build/iOS"
-FRAMEWORKS=`find "${FRAMEWORK_DIR}" -type d -name "*.framework"`
-
-for FRAMEWORK in ${FRAMEWORKS};
-    do
-    if [ -d "${FRAMEWORK}" ]; then
-		codesign --deep -f -s - "${FRAMEWORK}"
-     fi
-done
