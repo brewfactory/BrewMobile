6aa80a3556819589111404c1edfdb925d7ca84c2
vasarhelyia <vasarhelyi.agi@gmail.com>
2015-08-28 21:52:44 +0200

    Remove RAC 2 utils for RAC bindings

--- BrewMobile/RACUtils/RAC.swift
+++ BrewMobile/RACUtils/RAC.swift
@@ -17,29 +17,6 @@ extension UITextField {
     }
 }
 
-// a struct that replaces the RAC macro
-struct RAC  {
-  var target : NSObject!
-  var keyPath : String!
-  var nilValue : AnyObject!
-  
-  init(_ target: NSObject!, _ keyPath: String, nilValue: AnyObject? = nil) {
-    self.target = target
-    self.keyPath = keyPath
-    self.nilValue = nilValue
-  }
-  
-  
-  func assignSignal(signal : RACSignal) {
-    signal.setKeyPath(self.keyPath, onObject: self.target, nilValue: self.nilValue)
-  }
-}
-
-infix operator ~> {}
-func ~> (signal: RACSignal, rac: RAC) {
-  rac.assignSignal(signal)
-}
-
 struct AssociationKey {
     static var hidden: UInt8 = 1
     static var date: UInt8 = 2
